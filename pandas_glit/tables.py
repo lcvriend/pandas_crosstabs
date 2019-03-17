5c7892a73319ff403e84f2cd43d9afaf1ba3ddcb
Lawrence Vriend <43742775+lcvriend@users.noreply.github.com>
2019-03-17 18:53:10 +0100

    Adding _add_agg (also works on level = 0)

--- pandas_glit/tables.py
+++ pandas_glit/tables.py
@@ -826,6 +826,151 @@ def _add_sub_agg(df, level, axis=0, agg='sum', label=None, round=1):
     return df_out
 
 
+def _add_agg(df, level, axis=0, agg='sum', label=None, round=1):
+    """
+    Main logic to the add_sub_agg function.
+    """
+
+    semantic_code = {
+        'sum': 't',
+        'count': 'c',
+        }
+    semantic_code = semantic_code.get(agg, 'a')
+
+    if label is None:
+        if isinstance(agg, str):
+            label = agg
+        else:
+            label = agg.__name__
+
+    original_dtypes = dict(zip(df.columns.values, df.dtypes.values))
+    df_out = copy_df(df, transpose=not axis)
+    df = df_out.copy()
+    col_semantics = df_out.semantics.col
+    row_semantics = df_out.semantics.row
+    nlevels = df.columns.nlevels
+
+    # DataFrame without MultiIndex
+    if nlevels == 1:
+        df_out[label] = return_semantics(df_out, axis=1).agg(agg, axis=1)
+        df_out.semantics.col.append('a')
+        if axis == 0:
+            df_out = df_out.T
+            df_out = df_out.astype(original_dtypes)
+            df_out.semantics.col = row_semantics
+            df_out.semantics.row = col_semantics
+        else:
+            df_out.semantics.col = col_semantics
+            df_out.semantics.row = row_semantics
+        return df_out
+
+    # DataFrame with MultiIndex
+    # set levels
+    if level > nlevels - 1:
+        raise Exception(
+            f'The index has {nlevels - 1} useable levels: '
+            f'{list(range(nlevels - 1))}. Level {level} is out of bounds.'
+            )
+    # nlevels += 1
+    # level += 1
+
+    # deal with categorical indexes
+    if df_out.columns.levels[level].dtype.name == 'category':
+        new_level = df_out.columns.levels[level].add_categories(label)
+        df_out.columns.set_levels(new_level, level=level, inplace=True)
+
+    i = level
+    while i < (nlevels - 1):
+        try:
+            new_level = df_out.columns.levels[i].add_categories('')
+            df_out.columns.set_levels(new_level, level=i, inplace=True)
+        except:
+            pass
+        i += 1
+
+    # collect columns with values and columns with content
+    content = ['v', 'p']
+    v_cols = [elem == 'v' for elem in col_semantics]
+    c_cols = [elem in content for elem in col_semantics]
+
+    # aggregation on level == 0
+    if level == 0:
+        empty = ('',) * (nlevels - 1)
+        key = label, *empty
+        df_out[key] = df.loc[:, v_cols].agg(agg, axis=1)
+        df_out.semantics.col.append('a')
+        if axis == 0:
+            df_out = df_out.T
+            df_out = df_out.astype(original_dtypes)
+            df_out.semantics.col = row_semantics
+            df_out.semantics.row = col_semantics
+        else:
+            df_out.semantics.col = col_semantics
+            df_out.semantics.row = row_semantics
+        return df_out
+
+    # aggregation on level > 0
+    # collect column keys for specified level
+    col_keys = list()
+    for col in df.loc[:, v_cols].columns.remove_unused_levels():
+        fnd_col = col[: level]
+        col_keys.append(fnd_col)
+    col_keys = list(dict.fromkeys(col_keys))
+
+    # select groups from table, sum them and add to df
+    level_list = list(range(level))
+    for key in col_keys:
+        # find last key in group
+        tbl_grp = (
+            df.loc[:, c_cols]
+              .xs([*key], axis=1, level=level_list, drop_level=False)
+            )
+
+        key_last_col = tbl_grp.iloc[:, -1].name
+        lst_last_col = list(key_last_col)
+        lst_last_col[level] = label
+
+        i = level
+        while i < (nlevels - 1):
+            lst_last_col[i] = ''
+            i += 1
+        key_new_col = tuple(lst_last_col)
+
+        # insert new column
+        idx_col = df_out.columns.get_loc(key_last_col) + 1
+        extended_cols = df_out.insert(idx_col, key_new_col, 0)
+        col_semantics.insert(idx_col, semantic_code)
+        df_out = pd.DataFrame(df_out, columns=extended_cols)
+
+        # aggregate and update
+        tbl_grp = (
+            df.loc[:, v_cols]
+              .xs([*key], axis=1, level=level_list, drop_level=False)
+            )
+
+        agg_vals = tbl_grp.agg(agg, axis=1).values
+        if round is not None:
+            agg_vals = agg_vals.round(round)
+        multiindex = pd.MultiIndex.from_tuples([key_new_col])
+        df_col = pd.DataFrame(
+            data=agg_vals,
+            columns=multiindex,
+            index=df.index,
+            )
+        df_out.update(df_col)
+
+    # transpose if aggregating on index and store semantics
+    if axis == 0:
+        df_out = df_out.T
+        df_out = df_out.astype(original_dtypes)
+        df_out.semantics.col = row_semantics
+        df_out.semantics.row = col_semantics
+    else:
+        df_out.semantics.col = col_semantics
+        df_out.semantics.row = row_semantics
+    return df_out
+
+
 def order_cat(df, categories):
     """
     Add ordered categories to a DataFrame.
