92253ca5fa165a7a86c39e92f6ce6da26a0d2903
Lawrence Vriend <43742775+lcvriend@users.noreply.github.com>
2019-03-17 09:09:17 +0100

    Rewriting FancyTable class (adding semantics)

--- pandas_glit/tables.py
+++ pandas_glit/tables.py
@@ -14,14 +14,25 @@ class FancyTable:
     """
     Class for adding custom styling to DataFrames.
     """
+    CLASS_VALUES_COL = {
+        'p': 'perc_col',
+        'T': 'totals_col',
+        'P': 'totals_perc_col',
+        'a': 'agg_col',
+    }
+    CLASS_VALUES_ROW = {
+        'T': 'totals_row',
+        'a': 'agg_row',
+    }
 
     path_to_css = Path(__file__).resolve().parent
     tab_len = 4
     tab = ' ' * tab_len
 
     def __init__(
-        self, df,
-        style='kindofblue',
+        self,
+        df,
+        style=None,
         edge_lvl_row=None,
         edge_lvl_col=None,
     ):
@@ -29,13 +40,20 @@ class FancyTable:
         self.nlevels_col = df.columns.nlevels
         self.nlevels_row = df.index.nlevels
         self.nrows, self.ncols = df.shape
-        self._style = style
-        self.style = self._style
+        self.styling = Style(style)
+        self.style = self.styling.style
+        self.css = self.styling.css
 
-        self._edge_lvl_row = self._lvl_find_value(
-            edge_lvl_row, 'row') if edge_lvl_row is not None else self._edge_lvl_row
-        self._edge_lvl_col = self._lvl_find_value(
-            edge_lvl_col, 'col') if edge_lvl_col is not None else self._edge_lvl_col
+        self._edge_lvl_row = (
+            self._lvl_find_value(edge_lvl_row, 'row')
+            if edge_lvl_row is not None
+            else self._lvl_find_value(self.styling.edge_lvl_row, 'row')
+            )
+        self._edge_lvl_col = (
+            self._lvl_find_value(edge_lvl_col, 'col')
+            if edge_lvl_col is not None
+            else self._lvl_find_value(self.styling.edge_lvl_col, 'col')
+            )
 
         self.row_edges = self.find_edges(
             self.df.index,
@@ -47,43 +65,6 @@ class FancyTable:
             self.nlevels_col,
             edge_level=self.edge_lvl_col,
             )
-
-    @property
-    def css(self):
-        style_path = self.path_to_css / f'table_{self.style}.css'
-        return style_path.read_text()
-
-    @property
-    def styles(self):
-        return list(self.path_to_css.glob('table_*.css'))
-
-    @property
-    def style(self):
-        return self._style
-
-    @style.setter
-    def style(self, value):
-        """
-        Setter for the style. Also loads the edge levels in a style if any are defined.
-        """
-
-        self._style = value
-
-        edge_lvls = {'row': None, 'col': None}
-        axes = ['row', 'col']
-        lines = self.css.split('\n', 4)[1:3]
-
-        for line in lines:
-            try:
-                elems = line.split('=')
-                for axis in axes:
-                    if axis in elems[0]:
-                        edge_lvls[axis] = elems[1]
-            except:
-                continue
-
-        self.edge_lvl_row = edge_lvls['row']
-        self.edge_lvl_col = edge_lvls['col']
 
     @property
     def edge_lvl_row(self):
@@ -116,7 +97,6 @@ class FancyTable:
         if value is None:
             return -1
         if isinstance(value, str):
-            value = value.strip()
             if value == 'nogrid':
                 value = 0
             elif value == 'full':
@@ -136,7 +116,10 @@ class FancyTable:
         return(f'<style>{self.css}</style>\n{self._html()}')
 
     def _html(self):
-        html_tbl = f'<table class="{self.style}">\n{self._thead()}{self._tbody()}</table>\n'
+        html_tbl = (
+            f'<table class="{self.style}">\n'
+            f'{self._thead()}{self._tbody()}</table>\n'
+        )
         return html_tbl
 
     def _thead(self):
@@ -149,13 +132,15 @@ class FancyTable:
 
             # column index
             col_idx_name = self.df.columns.get_level_values(i).name
-            if col_idx_name == None:
+            if col_idx_name is None:
                 col_idx_name = ''
             colspan = ''
             if self.nlevels_row > 1:
                 colspan = f' colspan="{self.nlevels_row}"'
 
-            html_repr = f'<th class="col_idx_name"{colspan}>{col_idx_name}</th>\n'
+            html_repr = (
+                f'<th class="col_idx_name"{colspan}>{col_idx_name}</th>\n'
+                )
             level.append(html_repr)
 
             # column names
@@ -170,7 +155,7 @@ class FancyTable:
 
         # row index names
         def html_repr_idx_names(idx_name):
-            if idx_name == None:
+            if idx_name is None:
                 idx_name = ''
             html_repr = f'<th class="row_idx_name">{idx_name}</td>\n'
             return html_repr
@@ -180,10 +165,18 @@ class FancyTable:
             level = [html_repr_idx_names(idx_name) for idx_name in idx_names]
 
             def html_repr_idx_post(col_idx, item):
-                col_edge = ''
+                classes = list()
                 if col_idx in self.col_edges:
-                    col_edge = ' col_edge'
-                html_repr = f'<td class="row_idx_post{col_edge}"></td>\n'
+                    classes.append('col_edge')
+                for key in self.CLASS_VALUES_COL:
+                    try:
+                        if self.df.semantics.col[col_idx] == key:
+                            classes.append(self.CLASS_VALUES_COL[key])
+                    except KeyError:
+                        pass
+                classes = ' '.join(classes)
+                classes = f' {classes}' if classes != '' else ''
+                html_repr = f'<td class="row_idx_post{classes}"></td>\n'
                 return html_repr
 
             level.extend(
@@ -216,17 +209,33 @@ class FancyTable:
 
         # values
         def html_repr(col_idx, item):
-            col_edge = ''
+            # find classes to add to col
+            classes = list()
             if col_idx in self.col_edges:
-                col_edge = ' col_edge'
-            html_repr = f'<td id="{tid}-{row_idx + 1}-{col_idx + 1}" class="tbl_cell{col_edge}">{item}</td>\n'
+                classes.append('col_edge')
+            for key in self.CLASS_VALUES_COL:
+                try:
+                    if self.df.semantics.col[col_idx] == key:
+                        classes.append(self.CLASS_VALUES_COL[key])
+                except TypeError:
+                    pass
+            classes = ' '.join(classes)
+            classes = f' {classes}' if classes != '' else ''
+
+            # write html line
+            html_repr = (
+                f'<td id="{tid}-{row_idx + 1}-{col_idx + 1}" '
+                f'class="tbl_cell{classes}">{item}</td>\n'
+                )
             return html_repr
 
         values = self.df.astype(str).values  # cast all values as strings
         row_vals = list()
         for row_idx, row in enumerate(values):
-            val_line = [html_repr(col_idx, item)
-                        for col_idx, item in enumerate(row)]
+            val_line = [
+                html_repr(col_idx, item)
+                for col_idx, item in enumerate(row)
+                ]
             val_line = (self.tab * 3).join(val_line)
             row_vals.append(val_line)
         row_elements.append(row_vals)
@@ -237,13 +246,24 @@ class FancyTable:
         # write tbody
         html = ''
         for idx, row in enumerate(rows):
-            edge = ''
+            # find classes to add to row
+            classes = list()
             if idx - 1 in self.row_edges:
-                edge = ' row_edge'
-            row_str = ''
-            row_str = (self.tab * 2) + f'<tr class="tbl_row{edge}">\n'
-            row_str += ''.join([(self.tab * 3) +
-                                item for item in row if item is not None])
+                classes.append('row_edge')
+            for key in self.CLASS_VALUES_ROW:
+                try:
+                    if self.df.semantics.row[idx] == key:
+                        classes.append(self.CLASS_VALUES_ROW[key])
+                except TypeError:
+                    pass
+            classes = ' '.join(classes)
+            classes = f' {classes}' if classes != '' else ''
+
+            # write html line
+            row_str = (self.tab * 2) + f'<tr class="tbl_row{classes}">\n'
+            row_str += ''.join(
+                [(self.tab * 3) + item for item in row if item is not None]
+                )
             row_str += (self.tab * 2) + '</tr>\n'
             html += row_str
         tbody = f'{self.tab}<tbody>\n{html}{self.tab}</tbody>\n'
@@ -261,7 +281,8 @@ class FancyTable:
 
     def set_idx_names(self, spans, level, axis=0):
         """
-        Return a list of html strings containing the index names. Does something slightly different depending on the axis.
+        Return a list of html strings containing the index names.
+        Does something different depending on the axis.
         """
 
         types = {
@@ -271,27 +292,43 @@ class FancyTable:
         prefix = types[axis]
         idx_names = list()
 
-        spn_idx = -1
+        spn_idx_left = 0
+        spn_idx_right = -1
         for span in spans:
-            spn_idx += span[1]
-            span_str = ''
             idx_name = span[0]
-            edge = ''
+            spn_idx_right += span[1]
+            spn_str = ''
+            classes = ''
 
             if span[1] > 1:
-                span_str = f' {prefix}span="{span[1]}"'
+                spn_str = f' {prefix}span="{span[1]}"'
             if isinstance(span[0], tuple):
                 idx_name = span[0][level]
 
-            if axis == 1 and spn_idx in self.col_edges:
-                edge = ' col_edge'
+            if axis == 1:
+                classes = list()
+                if spn_idx_right in self.col_edges:
+                    classes.append('col_edge')
+                for key in self.CLASS_VALUES_COL:
+                    try:
+                        if self.df.semantics.col[spn_idx_left] == key:
+                            classes.append(self.CLASS_VALUES_COL[key])
+                    except TypeError:
+                        pass
+                classes = ' '.join(classes)
+                classes = f' {classes}' if classes != '' else ''
 
-            html_repr = f'<th class="{prefix}_name{edge}"{span_str}>{idx_name}</th>\n'
+            html_repr = (
+                f'<th class="{prefix}_name{classes}"'
+                f'{spn_str}>{idx_name}</th>\n'
+                )
             idx_names.append(html_repr)
 
             if axis == 0:
                 nones = [None] * (span[1] - 1)
                 idx_names.extend(nones)
+
+            spn_idx_left += span[1]
         return idx_names
 
     def find_edges(self, index, nlevels, edge_level=-1):
@@ -328,7 +365,7 @@ class FancyTable:
                     val_tup = spans.pop(-1)
                     new_val_tup = val, val_tup[1] + 1
                     spans.append(new_val_tup)
-            except:
+            except IndexError:
                 spans.append((val, 1))
         return spans
 
